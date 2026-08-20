38c6a130b6e89fb236c1717b865d14991f5c81e4
Vedant Sharma <svedant210@gmail.com>
2026-08-20 18:46:29 +0530

    add auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify'
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import { z } from 'zod'
 import sql from '../db/client.js'
-import { th } from 'zod/v4/locales'
 
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
 const registerSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
@@ -12,7 +16,7 @@ const registerSchema = z.object({
 export const authRoutes = (app: FastifyInstance) => {
   app.post('/auth/register', async (req, reply) => {
     const result = registerSchema.safeParse(req.body);
-    if(!result.success) {
+    if (!result.success) {
       return reply.status(400).send(result.error)
     }
     const { email, password } = result.data;
@@ -31,11 +35,34 @@ export const authRoutes = (app: FastifyInstance) => {
         return newUser;
       });
       return reply.status(201).send({ user_id: user.user_id, email });
-    } catch (err:any) {
-      if(err.code === '23505') {
+    } catch (err: any) {
+      if (err.code === '23505') {
         return reply.status(409).send({ message: 'Email already exists' });
       }
       throw err;
     }
-  })
+  });
+
+  app.post('/auth/login', async (req, reply) => {
+    const result = loginSchema.safeParse(req.body);
+    if (!result.success) {
+      return reply.status(400).send(result.error)
+    }
+    const { email, password } = result.data;
+    const user = await sql<{ user_id: string, password_hash: string }[]>`
+      SELECT user_id, password_hash
+      FROM wallet.users
+      WHERE email = ${email}
+    `
+    if (user.length === 0) {
+      return reply.status(401).send({ message: 'Invalid credentials' });
+    }
+    const [foundUser] = user;
+    const passwordMatch = await bcrypt.compare(password, foundUser.password_hash);
+    if (!passwordMatch) {
+      return reply.status(401).send({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ user_id: foundUser.user_id }, process.env.JWT_SECRET as string);
+    return reply.status(200).send({ user_id: foundUser.user_id, token });
+  });
 }
